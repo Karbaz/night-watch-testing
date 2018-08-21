250fba63588a18d47ffe3450a9fc58e30ef6e946
arbaz khan <arbaz.k@bewakoof.com>
2018-08-21 10:48:13 +0530

    done with img src and titile

---
+++ images_title/index.js
@@ -0,0 +1,30 @@
+var structured_urls = require("./urls")
+var cheerio = require("cheerio")
+var seo_test_cases = {};
+
+
+Object.keys(structured_urls.structured_urls).map((value, index) => {
+    let current_query = structured_urls.structured_urls[value]
+    let copy = Object.assign({
+        [`${index+1}: ${current_query["tags"]}`]: function (browser) {
+            browser.resizeWindow(400, 400);
+            browser.url(current_query["url"]);
+            browser.waitForElementVisible('body', 1000);
+            browser.pause(1000);
+            browser.source(function (res) {
+                $ = cheerio.load(res.value)
+                var scripts = $("img")
+                scripts.map((index,val)=>{
+                    if(val.name === "img"){
+                        this.assert.ok(val.attribs.title,val.attribs.src)
+                    }
+                })
+            })
+            browser.end()
+        },
+    }, seo_test_cases)
+    seo_test_cases = copy;
+})
+
+
+exports.module = seo_test_cases;
