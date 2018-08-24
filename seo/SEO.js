f49da27eb2da7d211716376ae9a61da7d7519605
arbaz khan <arbaz.k@bewakoof.com>
2018-08-24 15:30:07 +0530

    working on test cases

--- seo/SEO.js
+++ seo/SEO.js
@@ -1,3 +1,4 @@
+var assert = require("assert")
 var seo_urls = require("../config/seo_urls")
 var seo_test_cases = {};
 seo_urls.seo_urls.map((value, index) => {
@@ -5,12 +6,12 @@ seo_urls.seo_urls.map((value, index) => {
         let copy = Object.assign({
             '@tags': [index == 1 ? "Example" : "Test_" + index.toString()],
             [value[testValue]['tag']]: function (browser) {
-                browser
-                    .resizeWindow(400, 400)
-                    .url(value[testValue]['url'])
-                    .waitForElementVisible('body', 1000)
-                    .pause(100)
-                    .end()
+                browser.resizeWindow(400, 400);
+                browser.url(value[testValue]['url']);
+                browser.waitForElementVisible('body', 1000);
+                browser.assert.title(value[testValue]['title'],"Working")
+                browser.pause(100);
+                browser.end();
             },
         }, seo_test_cases)
         seo_test_cases = copy;
