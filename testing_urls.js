cbaad0c06cc9b32c80f513845b34efc6504ed2f2
arbaz khan <arbaz.k@bewakoof.com>
2018-09-03 16:08:57 +0530

    working

--- testing_urls.js
+++ testing_urls.js
@@ -1,3 +1,196 @@
+var liveDomain = require("./global").liveDomain;
 exports.urls = {
-    
+
+
+    "SEO": {
+        "mobile": {
+            "url": `${liveDomain}/mobile-covers-india`,
+            "title": "Mobile Covers – Buy Phone Covers at ₹199 | Bewakoof.com",
+            "desc": "You'd want to buy a new Mobile Cover every day when you can get Phone Covers starting at ₹199 with 300+ printed Mobile Back Cover options. These Phone Cases are pocket-friendly yet super-sturdy & stylish. So grab Mobile Cases & Phone Covers Online at Bewakoof.com! ✓COD ✓Free Shipping ✓Easy Returns",
+            "tag": "MOBILE_LANDING_PAGE"
+        },
+        "home": {
+            "url": `${liveDomain}/`,
+            "title": "Online Fashion Shopping for Men, Women, Accessories - Bewakoof.com",
+            "desc": "Online Shopping Site for Men, Women & Accessories. Choose from the best in Men Clothing and Women Fashion Wear. Trendiest designs and styles available at Bewakoof.com. Select from a diverse range of clothes online. Shop from the best online shopping site for clothes. Free Shipping, Easy Returns, COD Available!",
+            "tag": "HOME_PAGE",
+        },
+        "mobile Brand": {
+            "url": `${liveDomain}/mobile-covers-india/apple-cases-back-covers`,
+            "title": "Apple - Buy Apple Cases & Covers Starting at Rs.199 | Bewakoof.com",
+            "desc": "Bewakoof.com brings you the largest collection of designer Apple cases in India starting at Rs.199. Buy funky Apple covers online for all phone brands in India.",
+            "tag": "MOBILE_BRANDING_PAGE"
+        },
+        "Mobile Product Page": {
+            "url": `${liveDomain}/iphone-x-back-covers-cases`,
+            "title": "iPhone x Cover - Buy iPhone x Cases India @ Rs.199 - Bewakoof.com",
+            "desc": "Buy iPhone X back cover online with Cool and Trendy designs at Bewakoof.com. Choose from extensive range of iPhone X cover, defender cases and iPhone X silicone case options starting at Rs.199. Free Shipping, Easy Returns, COD Available!",
+            "tag": "MOBILE_PRODUCT_PAGE"
+        },
+        "Design Of The Day": {
+            "url": `${liveDomain}/design-of-the-day`,
+            "title": "Design of The Day - Buy Latest Designs at Rs. 249 - Bewakoof.com",
+            "desc": "Buy best selling design of the day at affordable at Bewakoof.com.",
+            "tag": "DESIGN_OF_THE_DAY"
+        },
+        "CTM": {
+            "url": `${liveDomain}/campaign/color-of-the-month`,
+            "title": "Color of the Month T-Shirts - Pine Green T shirts at Rs.225 - Bewakoof.com",
+            "desc": "Shop Color Of The Month -Pine Green T-shirts starting at Rs.225. The special launch price lasts for 24 hours only. Find a new color on the 1st of each month.",
+            "tag": "COLOR_OF_THE_MONTH"
+        },
+        "DELIGHTS": {
+            "url": `${liveDomain}/campaign/delights-coupons-discounts-offers-sale`,
+            "title": "BEWAKOOF Coupons, Offers & Deals just for you!",
+            "desc": "Delightful deals and discounts are available on our website for you to avail of! Our customers are a priority for us and we wish to serve them the best deals so that your shopping experiences are great! Keep your eyes peeled for awesome deals and sale on Bewakoof merchandise, and shop for printed tees and funky collections from our store!",
+            "tag": "DELIGHTS"
+        },
+        "CONTACT US": {
+            "url": `${liveDomain}/contact-us`,
+            "title": "Bewakoof Customer Care Contact Support Help Line - Bewakoof.com",
+            "desc": "Have a query, dear customer? Not to worry!  Reach out to us at care@bewakoof.com as we dont have a customer care number, contact number and helpline number. We will get back to you within 24 hours and well surely address all your queries. Feel free to get in touch with us. Cant wait to interact with",
+            "tag": "CONTACT_US"
+        },
+        "BLOG": {
+            "url": `${liveDomain}/blog`,
+            "title": "The Bewakoof Blog - Style, scoop and more!",
+            "desc": "From discovering what's in vogue, to insights on the industry, fabulous feature articles, and all the inside scoop, this is your go-to place for all things Bewakoof!",
+            "tag": "BLOG"
+        },
+        "CTM": {
+            "url": `${liveDomain}/campaign/color-of-the-month`,
+            "title": "Color of the Month T-Shirts - Burgundy T shirts at Rs.225 - Bewakoof.com",
+            "desc": "Shop Color Of The Month - Burgundy T-shirts starting at Rs.225. The special launch price lasts for 24 hours only. Find a new color on the 1st of each month.",
+            "tag": "COLOR_OF_THE_MONTH"
+        },
+    },
+
+
+
+    "CANONICAL": {
+        "product_page": {
+            "url": `${liveDomain}/p/pine-green-boyfriend-t-shirt-for-women?src=collection`,
+            "tags": "PRODUCT PAGE",
+            "CanonicalLink": "https://www.bewakoof.com/p/pine-green-boyfriend-t-shirt-for-women"
+        },
+        "category_page": {
+            "url": `${liveDomain}/women-new-arrivals-collection`,
+            "tags": "CATEGORY PAGE",
+            "CanonicalLink": "https://www.bewakoof.com/women-new-arrivals-collection"
+        },
+        "color_of_the_month": {
+            "url": `${liveDomain}/cotm-collection`,
+            "tags": "COLOR OF THE MONTH",
+            "CanonicalLink": "cotm-collection"
+        },
+        "home": {
+            "url": `${liveDomain}`,
+            "tags": "BEWAKOOF HOME PAGE",
+            "CanonicalLink": "https://www.bewakoof.com"
+        },
+    },
+
+
+
+    "IMAGES_TITLE_ALT_TAGS": {
+        "product_page": {
+            "url": `${liveDomain}/p/pine-green-boyfriend-t-shirt-for-women?src=collection`,
+            "check": ["BreadcrumbList", "Organization", "Product"],
+            "tags": "PRODUCT PAGE"
+        },
+        "category_page": {
+            "url": `${liveDomain}/women-new-arrivals-collection`,
+            "check": ["BreadcrumbList", "Organization"],
+            "tags": "CATEGORY PAGE"
+        },
+        "mobile_page": {
+            "url": `${liveDomain}/mobile-covers-india`,
+            "check": ["BreadcrumbList", "Organization"],
+            "tags": "MOBILE PAGE"
+        },
+        "mobile_brand_page": {
+            "url": `${liveDomain}/mobile-covers-india/apple-cases-back-covers`,
+            "check": ["BreadcrumbList", "Organization"],
+            "tags": "MOBILE BRAND PAGE"
+        },
+        "all_pages": {
+            "url": `${liveDomain}`,
+            "tags": "ALL PAGES"
+        },
+    },
+
+
+
+    "NEXT_AND_PREV_LINK": {
+        "Link_1": {
+            "url": `${liveDomain}/women-new-arrivals-collection`,
+            "tags": "CHECKING FOR NEXT TAG",
+            "testCase": ["next", "prev"]
+        },
+        "Link_2": {
+            "url": `${liveDomain}/women-new-arrivals-collection/default/2`,
+            "tags": "CHECKING FOR NEXT TAG",
+            "testCase": ["prev", "next"]
+        },
+        "Link_3": {
+            "url": `${liveDomain}/women-new-arrivals-collection/default/3`,
+            "tags": "CHECKING FOR NEXT TAG",
+            "testCase": ["prev", "next"]
+        }
+    },
+
+
+
+    "STRUCTURE_DATA": {
+        "product_page": {
+            "url": `${liveDomain}/p/pine-green-boyfriend-t-shirt-for-women?src=collection`,
+            "check": ["BreadcrumbList", "Organization", "Product"],
+            "tags": "PRODUCT PAGE"
+        },
+        "category_page": {
+            "url": `${liveDomain}/women-new-arrivals-collection`,
+            "check": ["BreadcrumbList", "Organization"],
+            "tags": "CATEGORY PAGE"
+        },
+        "mobile_page": {
+            "url": `${liveDomain}/mobile-covers-india`,
+            "check": ["BreadcrumbList", "Organization"],
+            "tags": "MOBILE PAGE"
+        },
+        "mobile_brand_page": {
+            "url": `${liveDomain}/mobile-covers-india/apple-cases-back-covers`,
+            "check": ["BreadcrumbList", "Organization"],
+            "tags": "MOBILE BRAND PAGE"
+        },
+        "all_pages": {
+            "url": `${liveDomain}`,
+            "check": ["Organization", "Error"],
+            "tags": "ALL PAGES"
+        },
+    },
+
+
+
+    "TAG_COUNTER": {
+        "product_page": {
+            "url": `${liveDomain}/p/pine-green-boyfriend-t-shirt-for-women?src=collection`,
+            "tags": "PRODUCT PAGE"
+        },
+        "category_page": {
+            "url": `${liveDomain}/women-new-arrivals-collection`,
+            "tags": "CATEGORY PAGE"
+        },
+        "mobile_page": {
+            "url": `${liveDomain}/mobile-covers-india`,
+            "tags": "MOBILE PAGE"
+        },
+        "mobile_brand_page": {
+            "url": `${liveDomain}/mobile-covers-india/apple-cases-back-covers`,
+            "tags": "MOBILE BRAND PAGE"
+        },
+        "all_pages": {
+            "url": `${liveDomain}`,
+            "tags": "ALL PAGES"
+        },
+    }
 }
